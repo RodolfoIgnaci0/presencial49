fbc57b38c5f42c2e49e21b97b0a0858ce9b69ca0
rodolfo <rodolfo.perezg0@gmail.com>
2017-11-07 22:02:57 -0300

    intento delete con ajax

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -46,10 +46,10 @@ class PostsController < ApplicationController
   # DELETE /posts/1.json
   def destroy
     @post.destroy
-    respond_to do |format|
-      format.html { redirect_to posts_url, notice: 'Post was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    #respond_to do |format|
+      #format.html { redirect_to posts_url, notice: 'Post was successfully destroyed.' }
+      #format.json { head :no_content }
+    #end
   end
 
   private
